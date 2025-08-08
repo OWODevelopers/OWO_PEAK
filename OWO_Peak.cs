1169b5292ca096934f61780d5eb4a2605881c51b
OWO Devs <developers.owo@gmail.com>
2025-08-08 14:47:46 +0200

    chore: vine climbing and adjusted priorities

--- OWO_Peak.cs
+++ OWO_Peak.cs
@@ -48,7 +48,7 @@ namespace OWO_PEAK
                     owoSkin.StopClimbing();
                     owoSkin.StopSlipping();
 
-                    owoSkin.Feel("Landing", 2);
+                    owoSkin.Feel("Landing", 3);
                 }
             }
 
@@ -60,7 +60,7 @@ namespace OWO_PEAK
                     return;
 
                 owoSkin.LOG("YOU FELL");
-                owoSkin.Feel("Fall");
+                owoSkin.Feel("Fall",3);
             }
 
             [HarmonyPatch(nameof(Character.RPCA_PassOut))]
@@ -83,7 +83,7 @@ namespace OWO_PEAK
 
                 owoSkin.LOG("YOU ARE DEAD");
                 owoSkin.StopHeartBeat();
-                owoSkin.Feel("Death");
+                owoSkin.Feel("Death",4);
             }
             [HarmonyPatch(typeof(Character), "RPCA_Revive", MethodType.Normal)]
             [HarmonyPostfix]
@@ -94,7 +94,7 @@ namespace OWO_PEAK
 
                 owoSkin.LOG("REVIVED!");
                 owoSkin.StopHeartBeat();
-                owoSkin.Feel("Revive");
+                owoSkin.Feel("Revive",3);
             }
 
             [HarmonyPatch(typeof(Character), "UnPassOutDone", MethodType.Normal)]
@@ -270,6 +270,38 @@ namespace OWO_PEAK
             }
         }
 
+        [HarmonyPatch(typeof(CharacterVineClimbing))]
+        internal static class CharacterVineClimbing_Patch
+        {
+            [HarmonyPatch(nameof(CharacterVineClimbing.GrabVineRpc))]
+            [HarmonyPostfix]
+            private static void StartClimbingVine(CharacterVineClimbing __instance, PhotonView ropeView, int segmentIndex)
+            {
+                Character character = Traverse.Create(__instance).Field("character").GetValue<Character>();
+                if (!character.IsLocal)
+                    return;
+
+                JungleVine component = ropeView.GetComponent<JungleVine>();
+                if ((UnityEngine.Object)component == (UnityEngine.Object)null)
+                    return;
+
+                owoSkin.LOG("START VINE CLIMBING!");
+                owoSkin.StartClimbingRope();
+            }
+
+            [HarmonyPatch(typeof(CharacterVineClimbing), "StopVineClimbingRpc", MethodType.Normal)]
+            [HarmonyPostfix]
+            private static void RPC_StopClimbingVine(CharacterVineClimbing __instance)
+            {
+                Character character = Traverse.Create(__instance).Field("character").GetValue<Character>();
+                if (!character.IsLocal)
+                    return;
+
+                owoSkin.LOG("STOP VINE CLIMBING!");
+                owoSkin.StopClimbingRope();
+            }
+        }
+
         [HarmonyPatch(typeof(CharacterCarrying))]
         internal static class CharacterCarrying_Patch
         {
@@ -379,177 +411,6 @@ namespace OWO_PEAK
                 owoSkin.StopTooting();
             }
         }
-
-        #region Legacy
-        //[HarmonyPatch(typeof(Attack), nameof(Attack.OnAttackTrigger))]
-        //class OnWeaponAttack
-        //{
-        //    public static Dictionary<string, float> rangeBoss = new Dictionary<string, float>()
-        //{
-        //    {"boss_eikthyr",  20f},
-        //    {"boss_gdking", 40f },
-        //    {"boss_bonemass",  20f},
-        //    {"boss_moder",  40f},
-        //    {"boss_goblinking",  60f},
-        //};
-        //    public static void Postfix(Attack __instance, Humanoid ___m_character, ItemDrop.ItemData ___m_weapon)
-        //    {
-        //        if (!owoSkin.CanFeel()) return;
-
-        //        if (___m_character.IsBoss()) goto Boss;
-
-        //        if (___m_character == Player.m_localPlayer) goto Player;
-
-        //        return;
-
-
-        //    Boss:
-        //        float range = (rangeBoss.ContainsKey(___m_character.m_bossEvent)) ? rangeBoss[___m_character.m_bossEvent] : 20f;
-        //        bool closeTo = Player.IsPlayerInRange(___m_character.transform.position, range, Player.m_localPlayer.GetPlayerID());
-
-        //        if (!closeTo) return;
-
-        //        switch (___m_character.m_bossEvent)
-        //        {
-        //            case "boss_eikthyr":
-        //                if (__instance.m_attackAnimation == "attack2")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                if (__instance.m_attackAnimation == "attack_stomp")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                break;
-        //            case "boss_gdking":
-        //                if (__instance.m_attackAnimation == "spawn")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                if (__instance.m_attackAnimation == "stomp")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                if (__instance.m_attackAnimation == "shoot")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                break;
-        //            case "boss_bonemass":
-        //                if (__instance.m_attackAnimation == "aoe")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                break;
-        //            case "boss_moder":
-        //                if (__instance.m_attackAnimation == "attack_iceball")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                if (__instance.m_attackAnimation == "attack_breath")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                break;
-        //            case "boss_goblinking":
-        //                if (__instance.m_attackAnimation == "beam")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                if (__instance.m_attackAnimation == "nova")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                if (__instance.m_attackAnimation == "cast1")
-        //                {
-        //                    owoSkin.Feel("Earthquake", 2);
-        //                }
-        //                break;
-
-        //        }
-        //        return;
-
-
-        //    Player:
-        //        owoSkin.LOG($"HUMANOID {___m_weapon.m_shared.m_itemType} -- {___m_weapon.m_shared.m_animationState} -- {___m_weapon.m_shared.m_name}");
-        //        switch (___m_weapon.m_shared.m_itemType)
-        //        {
-        //            case ItemDrop.ItemData.ItemType.TwoHandedWeapon:
-        //            case ItemDrop.ItemData.ItemType.Bow:
-        //                owoSkin.FeelWithMuscles("Attack","Both Arms", 3);
-        //                break;
-        //            default:
-        //                owoSkin.FeelWithMuscles("Attack","Right Arm", 3);
-        //                break;
-        //        }
-        //        return;
-        //    }
-        //}
-
-        //[HarmonyPatch(typeof(Character), "SetHealth")]
-        //class OnPlayerSetHealth
-        //{
-        //    public static void Postfix(Character __instance)
-        //    {
-        //        if (__instance != Player.m_localPlayer || !owoSkin.CanFeel()) return;
-        //        int hp = Convert.ToInt32(__instance.GetHealth() * 100 / __instance.GetMaxHealth());
-
-        //        if (hp <= 0)
-        //        {
-        //            owoSkin.StopAllHapticFeedback();
-        //            owoSkin.playerEnabled = false;
-        //            owoSkin.Feel("Death", 4);
-        //        }
-        //        else if (hp < 20 && hp > 0)
-        //        {
-        //            owoSkin.StartHeartBeat();
-        //        }
-        //        else
-        //        {
-        //            owoSkin.StopHeartBeat();
-        //        }
-        //    }
-        //}
-
-
-
-        //[HarmonyPatch(typeof(Ragdoll), "DestroyNow")]
-        //class OnCorpseExplosion
-        //{
-        //    public static void Postfix(Ragdoll __instance)
-        //    {
-        //        if (!owoSkin.CanFeel() || !Player.IsPlayerInRange(__instance.transform.position, 20f, Player.m_localPlayer.GetPlayerID())) return;
-        //        foreach (EffectList.EffectData obj in __instance.m_removeEffect.m_effectPrefabs)
-        //        {
-        //            switch (obj.m_prefab.name)
-        //            {
-        //                case "vfx_corpse_destruction_medium":
-        //                    goto SendSensation;
-        //                case "vfx_corpse_destruction_small":
-        //                    if (!Player.IsPlayerInRange(__instance.transform.position, 7f, Player.m_localPlayer.GetPlayerID())) return;
-        //                    goto SendSensation;
-        //            }
-        //        }
-
-        //    SendSensation:
-        //        owoSkin.Feel("Explosion", 2);
-        //    }
-        //}
-
-        //[HarmonyPatch(typeof(Character), "OnCollisionStay")]
-        //class OnPlayerLand
-        //{
-        //    public static void Postfix(Player __instance, bool ___m_groundContact)
-        //    {
-        //        if (!owoSkin.CanFeel() || __instance != Player.m_localPlayer) return;
-        //        if (owoSkin.isJumping && ___m_groundContact)
-        //        {
-        //            owoSkin.isJumping = false;
-        //            owoSkin.Feel("Landing");
-        //        }
-        //    }
-        //} 
-        #endregion
     }
 }
 
